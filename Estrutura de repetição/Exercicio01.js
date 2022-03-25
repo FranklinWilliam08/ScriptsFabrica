47e815d9d814f287bf32f5aa2e19e5b3454d0792
FranklinWilliam08 <franklinarchenemy08@gmail.com>
2022-03-25 19:39:11 -0300

    Primeiro commit

---
+++ Estrutura de repetição/Exercicio01.js
@@ -0,0 +1,41 @@
+
+
+
+
+let nome = prompt('Informe seu nome: ');
+
+
+alert('Seja bem vindo ao site '+nome);
+console.log('Seja bem vindo '+nome);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let nomes = []
+
+nomes[0] = 'matheus';
+nomes[1] = 'William';
+nomes.push('Franklin');
+
+console.log(nomes)
+
+/*nomes[0] = 'Franklin William'
+nomes[1] = 'Matheus'
+nomes[2] = 'William'
+
+console.log(nomes)*/
+
+
+
+
+//var number1 = 10;
+
